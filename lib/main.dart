4e9031e0d2daf442088c2f8dc15008f9e03206af
DairikuYoko <yokoriku@gmail.com>
2025-05-21 00:39:19 +0900

    secton4スタート。Scaffold,Columnについて学んだ

--- lib/main.dart
+++ lib/main.dart
@@ -42,29 +42,33 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+        appBar: AppBar(
+          backgroundColor: Theme
+              .of(context)
+              .colorScheme
+              .inversePrimary,
+          title: Text(widget.title),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Container(
+              color: Colors.blue,
+              width: 200,
+              height: 200,
+            ),
+            Container(
+              color: Colors.yellow,
+              width: 150,
+              height: 150,
+            ),
+            Container(
+              color: Colors.red,
+              width: 100,
+              height: 100,
+            )
+          ],
+        )
     );
   }
 }
