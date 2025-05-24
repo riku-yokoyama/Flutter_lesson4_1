981d86c2af7eb55ddff6fb2ec0159961886aee24
DairikuYoko <yokoriku@gmail.com>
2025-05-25 03:34:37 +0900

    section4の課題1, ログイン画面の作成完了

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.indigoAccent),
         useMaterial3: true,
       ),
       home: const MyHomePage(title: 'Flutter Lesson'),
@@ -41,39 +41,81 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          backgroundColor: Theme
-              .of(context)
-              .colorScheme
-              .inversePrimary,
-          title: Text(widget.title),
-        ),
-
-        body: Center(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Padding(
-                padding: const EdgeInsets.only(left: 0.10),
-                child: Text(
-                  "パスワード"
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          // パスワード入力欄
+          SizedBox(
+            width: 230,
+            height: 40,
+            child: TextField(
+              decoration: InputDecoration(
+                prefixIcon: Icon(
+                  Icons.key_rounded,
+                  size: 20,
+                ),
+                hintText: "Password",
+                border: OutlineInputBorder(),
+              ),
+            ),
+          ),
+
+          // E-mail入力欄
+          Padding(
+            padding: const EdgeInsets.only(top: 20),
+            child: SizedBox(
+              width: 230,
+              height: 40,
+              child: TextField(
+                decoration: InputDecoration(
+                  prefixIcon: Icon(
+                    Icons.mail_outline,
+                    size: 20,
+                  ),
+                  hintText: "E-mail",
+                  border: OutlineInputBorder(),
                 ),
               ),
-              Row(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  SizedBox(
-                    width: 200,
-                    child: TextField(
-                      decoration: InputDecoration(
-                        border: OutlineInputBorder(),
-                      ),
-                    ),
-                  )
-                ],
-              )
-            ],
+            ),
           ),
-        ));
+
+          // ログインボタン
+          Padding(
+            padding: const EdgeInsets.only(top: 90),
+            child: SizedBox(
+              width: 230,
+              height: 40,
+              child: ElevatedButton(
+                  onPressed: () {
+                    showDialog(
+                        context: context,
+                        builder: (context) {
+                          return AlertDialog(
+                            title: Text(
+                                "ログイン成功",
+                                style: TextStyle(
+                                  fontSize: 18
+                                ),
+                            ),
+                          );
+                        });
+                  },
+                  style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.indigoAccent[100]),
+                  child: Text(
+                    "Login",
+                    style: TextStyle(color: Colors.white, fontSize: 17),
+                  )),
+            ),
+          )
+        ],
+      )),
+    );
   }
 }
