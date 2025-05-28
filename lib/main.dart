46fbdf22524e6ef68e45b31d4bd26f1e3cb46803
DairikuYoko <yokoriku@gmail.com>
2025-05-29 01:31:31 +0900

    secton4の課題2完了-電話帳の作成-

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.indigoAccent),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Lesson'),
+      home: const MyHomePage(title: '連絡先'),
     );
   }
 }
@@ -30,92 +30,71 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-          child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          // パスワード入力欄
-          SizedBox(
-            width: 230,
-            height: 40,
-            child: TextField(
-              decoration: InputDecoration(
-                prefixIcon: Icon(
-                  Icons.key_rounded,
-                  size: 20,
-                ),
-                hintText: "Password",
-                border: OutlineInputBorder(),
-              ),
-            ),
-          ),
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          title: Text(widget.title),
+        ),
+        body: PhoneListView());
+  }
+}
 
-          // E-mail入力欄
-          Padding(
-            padding: const EdgeInsets.only(top: 20),
-            child: SizedBox(
-              width: 230,
-              height: 40,
-              child: TextField(
-                decoration: InputDecoration(
-                  prefixIcon: Icon(
-                    Icons.mail_outline,
-                    size: 20,
-                  ),
-                  hintText: "E-mail",
-                  border: OutlineInputBorder(),
-                ),
-              ),
-            ),
-          ),
+// 電話帳画面の本体のカスタムウィジェット（ListViewを返す)
+class PhoneListView extends StatelessWidget {
+  const PhoneListView({
+    super.key,
+  });
 
-          // ログインボタン
-          Padding(
-            padding: const EdgeInsets.only(top: 90),
-            child: SizedBox(
-              width: 230,
-              height: 40,
-              child: ElevatedButton(
-                  onPressed: () {
-                    showDialog(
-                        context: context,
-                        builder: (context) {
-                          return AlertDialog(
-                            title: Text(
-                                "ログイン成功",
-                                style: TextStyle(
-                                  fontSize: 18
-                                ),
-                            ),
-                          );
-                        });
-                  },
-                  style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.indigoAccent[100]),
-                  child: Text(
-                    "Login",
-                    style: TextStyle(color: Colors.white, fontSize: 17),
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      children: [
+        Container(
+          height: 80,
+          padding: EdgeInsets.only(top: 10),
+          child: ListTile(
+              contentPadding: EdgeInsets.symmetric(horizontal: 0),
+              minLeadingWidth: 10,
+              // 横の余白をゼロに
+              leading: CircleAvatar(
+                  radius: 40,
+                  child: Icon(
+                    Icons.person,
+                    size: 45,
                   )),
-            ),
-          )
-        ],
-      )),
+              title: Text('りく'),
+              subtitle: Text('マイカード'),
+              trailing: Icon(Icons.arrow_forward_ios)),
+        ),
+        Divider(),
+        ListTile(
+            leading: Icon(Icons.phone),
+            title: Text('山田 太郎'),
+            subtitle: Text('070-1234-567'),
+            trailing: Icon(Icons.arrow_forward_ios)),
+        ListTile(
+            leading: Icon(Icons.phone),
+            title: Text('鈴木一郎'),
+            subtitle: Text('070-1234-567'),
+            trailing: Icon(Icons.arrow_forward_ios)),
+        ListTile(
+            leading: Icon(Icons.phone),
+            title: Text('佐藤 花子'),
+            subtitle: Text('070-1234-567'),
+            trailing: Icon(Icons.arrow_forward_ios)),
+        ListTile(
+            leading: Icon(Icons.phone),
+            title: Text('桜井 和寿'),
+            subtitle: Text('070-1234-567'),
+            trailing: Icon(Icons.arrow_forward_ios)),
+        ListTile(
+            leading: Icon(Icons.phone),
+            title: Text('丹羽 長秀'),
+            subtitle: Text('070-1234-567'),
+            trailing: Icon(Icons.arrow_forward_ios))
+      ],
     );
   }
 }
